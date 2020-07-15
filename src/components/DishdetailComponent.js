f934cf47eba6ac982f8976deb046df7fe9fe1b0c
Anirudh Giri <anirudhges@gmail.com>
2020-07-16 00:52:17 +0530

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody,
     CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { LocalForm, Control, Errors } from 'react-redux-form';
+import {Loading} from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -160,7 +161,28 @@ const minLength = (len) => (val) => val && (val.length >= len);
     const DishDetail = (props) => {
         let dish = props.dish;
         let comments = null;
-        if(dish != null)
+
+        if(props.isLoading){
+            return(
+                <div className="container">
+                    <div className="row">
+                        <Loading/>
+                    </div>
+                </div>
+            );
+        }
+
+        else if(props.errMess){
+            return(
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+
+        else if(dish != null)
             comments = dish.comments;
         return(
             <div className="container">
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import {Link} from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 function RenderMenuItem({dish, onClick}){
     return(
@@ -16,30 +17,52 @@ function RenderMenuItem({dish, onClick}){
 }
 
 const Menu = (props) => {
-    const menu = props.dishes.map(dish => {
+    const menu = props.dishes.dishes.map(dish => {
         return (
             <div key={dish.id} className="col-12 col-md-5 m-1">
                 <RenderMenuItem dish = {dish}/>
             </div>
         );
     });
+    if (props.dishes.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.dishes.errMess) {
+        return(
+            <div className="container">
+                <div className="row"> 
+                    <div className="col-12">
+                        <h4>{props.dishes.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
     
-    return(
-        <div className="container">
-            <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                    <BreadcrumbItem active><Link to="/menu">Menu</Link></BreadcrumbItem>
-                </Breadcrumb>
+    else{
+        return(
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    </Breadcrumb>
+                </div>
+                <div className="col-12">
+                    <h3>Menu</h3>
+                </div>
+                <div className="row">
+                        {menu}
+                </div>
             </div>
-            <div className="col-12">
-                <h3>Menu</h3>
-            </div>
-            <div className="row">
-                    {menu}
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Menu;
