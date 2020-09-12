e423203d63b07b97012c8f376a5836c8b529aa1c
Anirudh Giri <anirudhges@gmail.com>
2020-09-12 15:58:04 +0530

    add production build

---
+++ build/precache-manifest.bd42f03967c5e76b40625151f467c69e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b71796845ae6e0e3b7ea966808140ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3a5e7245f8868cc471b",
+    "url": "/static/css/2.1bd367ea.chunk.css"
+  },
+  {
+    "revision": "9a59016f5de85611e059",
+    "url": "/static/css/main.17322a8a.chunk.css"
+  },
+  {
+    "revision": "e3a5e7245f8868cc471b",
+    "url": "/static/js/2.a5186d93.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a5186d93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a59016f5de85611e059",
+    "url": "/static/js/main.38489470.chunk.js"
+  },
+  {
+    "revision": "d2e3cb96056d7d0b1a93",
+    "url": "/static/js/runtime-main.5100553d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
